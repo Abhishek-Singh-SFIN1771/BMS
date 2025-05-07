21f38abe43ba8fdea8dfe16343a37795bb0b7165
Abhishek Singh <singhabhishek696932@gmail.com>
2025-05-07 17:51:55 +0530

    JavaScripy File Of BMS

--- BMS.js
+++ BMS.js
@@ -1,2 +1,10 @@
 // Javascript is a technology we used to create websites
 // sites like -  youtube.com , amamzom.com
+// javascript makes our website interactive
+
+alert('hello'); // this is used to give a popup on our website
+
+document.body.innerHTML = 'hello'; // it replace all thing on page with the value given isnide it.
+
+console.log(Math.round(2.34)); // syntax to print on page
+
