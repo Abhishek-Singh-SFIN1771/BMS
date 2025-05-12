88d791de3752d18820d8f9174c7e238a2f5d74b9
Abhishek Singh <singhabhishek696932@gmail.com>
2025-05-12 21:08:12 +0530

    Different files for codes

--- BMS.js
+++ BMS.js
@@ -1,12 +1,48 @@
-// Javascript is a technology we used to create websites
-// sites like -  youtube.com , amamzom.com
-// javascript makes our website interactive
+document.getElementById('Book-Form').
+        addEventListener('submit' , (e) => 
+        {
+            e.preventDefault();
 
+            const titile = document.getElementById('title').value.trim();
+            const author = document.getElementById('author').value.trim();
+            const isbn = document.getElementById('isbn').value.trim();
+            const date = document.getElementById('year').value.trim();
+            const genre = document.getElementById('genre').value.trim();
 
-// Learnin of JS happen here
-alert('hello'); // this is used to give a popup on our website
+            if(!titile || !author || !isbn || !year || !genre)
+            {
+                alert("Make sure to fill all fields");
+                return;
+            }
 
-document.body.innerHTML = 'hello'; // it replace all thing on page with the value given isnide it.
+            if (isNaN(isbn)) {
+                alert("ISBN should be a number.");
+                return;
+            }
 
-console.log(Math.round(2.34)); // syntax to print on page
+        });
 
+class Book
+{
+    constructor(title, author, isbn, year, genre) 
+    {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.year = parseInt(year);
+    this.genre = genre;
+    }
+    
+}
+
+function calculateAge ()
+{
+    const publishYear = parseInt(document.getElementById('year').value.trim());
+    
+    const currentYear = new Date().getFullYear();
+
+    const age = currentYear - publishYear;
+    
+    const ageInput = document.querySelector('.js-age-input');
+    ageInput.value = age + ' years';
+}
