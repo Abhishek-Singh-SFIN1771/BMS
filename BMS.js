36482e4718bcfa0306327c185a4eca32ffd2671c
Abhishek Singh <singhabhishek696932@gmail.com>
2025-05-16 13:16:27 +0530

    BMS with category, calculate age , interactive and responsive UI, oops concept , es6 feutres

--- BMS.js
+++ BMS.js
@@ -1,23 +1,34 @@
-const bookList = [];
+class Book
+{
+    bookList = [];
+    age = undefined;
 
-const bmsObject = {    
+    constructor()
+    {
+        this.start();
+    }
 
-    age: null,
-
-    addBookToList()
+    start()  // Valadating and calling add function.
     {
         const form = document.getElementById('book-form');
-        form.addEventListener('submit' , (e) =>{
-        e.preventDefault();
+        form.addEventListener('submit' , 
+            (e) => {e.preventDefault();
 
-        // slecting querry form hrml into variables
+        const book = this.querrySelector();
+        this.addBookToList(book);
+        form.reset();
+        });
+    }
+
+    querrySelector() // returning book object with values from html page.
+    {
+        // slecting querry from html into variables
         const titleElement = document.querySelector('.title');
         const authorElement = document.querySelector('.author');
         const isbnElement = document.querySelector('.isbn');
         const yearElement = document.querySelector('.year');
         const genreElement = document.querySelector('.genre');
-
-    
+        
         // putting querry values into a varable
         const title = titleElement.value;
         const author = authorElement.value;
@@ -25,102 +36,99 @@ const bmsObject = {
         const year = yearElement.value;
         const genre = genreElement.value;
 
-        const book = {title , author , isbn , year, genre};
-
-        if ( !title || !author || !isbn || !year || !genre) 
-            {
-                alert('Add all fields before adding the books');
-                return;
-            }
-        else
-        {
-            bookList.push(book);
-            age = this.calculateAge();
-            titleElement.value= '';
-            authorElement.value = '';
-            isbnElement.value = '';
-            yearElement.value = '';
-            genreElement.value = '';
-
-            this.renderBooks();
-            
+        // Validation 
+        if (!title || !author || !isbn || !year || !genre) {
+           return alert('Fill all the fields');
         }
-    });
-    },
+        else{
+        const book = 
+        {title : title, author : author, isbn : isbn, year : year, genre : genre};
 
-    renderBooks()
+        return book;
+        }
+    }
+
+
+    addBookToList(book)
     {
-        const tbody = document.querySelector('.book-table');
-        tbody.innerHTML='';
-
-        bookList.forEach((book , index) => 
-            {
-                const category = this.categorizedGenre(book.genre); 
-                const row =  
-                `
-                <tr>
-                    <td data-label="title">${book.title}</td>
-                    <td data-label="author">${book.author}</td>
-                    <td data-label="isbn">${book.isbn}</td>
-                    <td data-label="year">${book.year}</td>
-                    <td data-label="year">${book.genre}</td>
-                    <td data-label="genre">${age}</td>
-                    <td data-label="category">${category}</td>
-
-                    <td data-label="Edit">    
-                        <button class="edit-button">Edit</button>
-                    </td>
-            
-                    <td data-label="Delete">    
-                        <button class="delete-button">Delete</button>
-                    </td>
-                </tr>
-                `
-                tbody.insertAdjacentHTML('beforeend', row);
-                
-            });  
-
-            
-        document.querySelectorAll('.delete-button')
-        .forEach((deleteButtonElement, index) =>
-        {
-            deleteButtonElement.addEventListener('click' , () => 
-                {
-                bookList.splice(index, 1); 
-                this.renderBooks();
-                });
-            deleteButtonElement.addEventListener('click' , () =>
-                {
-                    alert('book is deleted');
-                });
-        });
-
-    },
-
-    editBook(bookList)
-    {
-
-        
-        
-        // const book = bookList[index];
-        // document.getElementById('title').value = book.title;
-        // document.getElementById('author').value = book.author;
-        // document.getElementById('isbn').value = book.isbn;
-        // document.getElementById('year').value = book.year;
-        // document.getElementById('genre').value = book.genre;
-
-        // bookList.splice(index, 1);
-        // this.renderBooks();
-    },
+        this.bookList.push(book);
+        alert('book added to the database');
+        this.age = this.calculateAge();
+        this.renderBooks();
+        return;
+    }
 
     calculateAge() 
     {
         const publishYear = parseInt(document.getElementById('year').value.trim());
         const currentYear = new Date().getFullYear();
 
-        const age = currentYear - publishYear;
-        return age;
-    },
+        const age1 = currentYear - publishYear;
+        return age1;
+    }
+
+    renderBooks()
+    {
+        let bookHtml = '';
+        const tbody = document.querySelector('.book-table');         tbody.innerHTML='';
+
+        for(let i = 0; i < this.bookList.length ; i++)
+            {
+                const bookObject = this.bookList[i];
+                const{title , author , isbn , year , genre} = bookObject;
+                const category = this.categorizedGenre(genre); 
+                const row =  
+                `
+                <tr>
+                    <td data-label="title">${title}</td>
+                    <td data-label="author">${author}</td>
+                    <td data-label="isbn">${isbn}</td>
+                    <td data-label="year">${year}</td>
+                    <td data-label="year">${genre}</td>
+                    <td data-label="genre">${this.age}</td>
+                    <td data-label="category">${category}</td>
+
+                    <td data-label="Edit">    
+                        <button type = "button" class="edit-button">Edit</button>
+                    </td>
+            
+                    <td data-label="Delete">    
+                        <button type = "button" class="delete-button">Delete</button>
+                    </td>
+                </tr>
+                `
+                bookHtml += row;
+            }  
+
+        tbody.insertAdjacentHTML('beforeend', bookHtml);
+            
+        document.querySelectorAll('.delete-button')
+        .forEach((deleteButtonElement, index) =>
+        {
+            deleteButtonElement.addEventListener('click',() => 
+                {
+                    this.deleteBook(index);
+                });
+        });
+
+        document.querySelectorAll('.edit-button')
+        .forEach((editButtonElement, index) =>
+        {
+           
+            editButtonElement.addEventListener('click',() => 
+                {
+                   this.editBook(index);
+                }); 
+        });
+    }
+
+    deleteBook(index) 
+    {
+        this.bookList.splice(index, 1);
+        setTimeout(
+            () => {alert('book is deleted')},500);
+        this.renderBooks();
+    }
 
     categorizedGenre(genre)
     {
@@ -137,11 +145,33 @@ const bmsObject = {
         };
 
         return genreMap[genre.toLowerCase()] || 'other';
-    },
-    
-};
+    }
 
-bmsObject.addBookToList();
+    editBook(index) 
+    {
+        const book = this.bookList[index];
+
+        document.querySelector('.title').value = book.title;
+        document.querySelector('.author').value = book.author;
+        document.querySelector('.isbn').value = book.isbn;
+        document.querySelector('.year').value = book.year;
+        document.querySelector('.genre').value = book.genre;
+
+
+        this.updateBook(index);
+        
+    }
+
+    updateBook(index)
+    {
+        this.bookList.splice(index, 1);
+    }
+
+}
+
+const button = document.querySelector('.add-button');
+button.addEventListener('click', new Book());
+
 
 
 
