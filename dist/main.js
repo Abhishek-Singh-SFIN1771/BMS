03d928f14731cd3ec5baa39013437f6fff7d8260
Abhishek Singh <singhabhishek696932@gmail.com>
2025-05-27 11:32:28 +0530

    Stop tracking dist folder and add to .gitignore

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// import { BookManager } from './BMS';
-// document.addEventListener('DOMContentLoaded', () => {
-//   new BookManager();
-// });
--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-export function LogMethod(target, propertyName, descriptor) {
-    const originalMethod = descriptor.value;
-    descriptor.value = function (...args) {
-        console.log(`Method: ${propertyName} | Args:`, args);
-        return originalMethod.apply(this, args);
-    };
-}
